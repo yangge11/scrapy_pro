bb6cab75f4c5fb9b3fc2fbe0779b0463865a8a59
曾洋 <cengyang@cengyangdeMacBook-Pro.local>
2019-12-08 21:24:10 +0800

    Version1.0.9. b站番剧采集demo

--- scrapy_auto/unit_test/demo.py
+++ scrapy_auto/unit_test/demo.py
@@ -5,8 +5,13 @@
 # @Software: PyCharm
 # @ToUse  :
 import json
+import re
 import urllib
+import ssl
 
+import requests
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 def json_demo():
     dict1 = {"url": "https://www.lanzous.com/b12583", "name": "精品广场舞83-2.zip精品广场舞83-1.zip"}
@@ -71,6 +76,76 @@ def urllib_demo():
     pass
 
 
+def requests_demo():
+    """
+    pip install requests
+    1.模拟进行requests的get和post请求
+    2.response的一些参数
+    response.status_code
+    response.text
+    response.content
+    案例：b站番剧爬虫
+    :return:
+    """
+    headers = {
+        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+        'accept': 'application/json, text/javascript, */*',
+    }
+
+    #知识点1： requests.get请求以及response.status_code、response.text、response.content、注意response.request的headers里面的ua
+    # url_get = 'https://api.bilibili.com/pgc/season/index/result?season_version=-1&area=-1&is_finish=-1&copyright=-1&season_status=-1&season_month=-1&year=-1&style_id=-1&order=3&st=1&sort=0&page=1&season_type=1&pagesize=20&type=1'
+    # response = requests.get(url=url_get,headers = headers)
+    # r_json = json.loads(response.text)
+
+    # 知识点2：requests.post()请求
+    # url_post = 'http://httpbin.org/post'
+    # data ={'tx': 'hello', 'sign': 'sdfjisd8126324dsfj'}
+    # response = requests.post(url=url_post, data=data,headers = headers)
+
+    # 知识点3：response.text（str字符串型）和response.content的区别（bytes二进制型），案例：下载图片,使用文件读写的方式保存图片
+    # response = requests.get('http://i0.hdslb.com/bfs/bangumi/f5d5f51b941c01f8b90b361b412dc75ecc2608d3.png',headers=headers)
+    # with open('demo.jpg', 'wb+') as f:
+    #     f.write(response.content)
+    pass
+
+
+def b_fanju_demo():
+    """
+    B站番剧爬虫demo
+    :return:
+    """
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+        'accept': 'application/json, text/javascript, */*',
+    }
+    # 1.确定数据来源：b站、番剧、需要的数据（封面、视频合集链接、视频当前更新集数、是否会员、追番人数、视频标题）
+    # 2.确定抓取入口https://api.bilibili.com/pgc/season/index/result?season_version=-1&area=-1&is_finish=-1
+    # &copyright=-1&season_status=-1&season_month=-1&year=-1&style_id=-1&order=3&st=1&sort=0&page=1&season_type=1&pagesize=20&type=1(需要分析数据各个请求参数)
+    # 3.确定解析方式：json
+    # 4.确定存储方式：txt文本
+    url = 'https://api.bilibili.com/pgc/season/index/result?season_version=-1&area=-1&is_finish=0&copyright=-1&season_status=-1&season_month=-1&year=-1&style_id=-1&order=3&st=1&sort=0&page={page}&season_type=1&pagesize=20&type=1'
+    for page in range(1, 10):
+        response = requests.get(url=url.format(page = 1), headers=headers)
+        dongman_list = json.loads(response.text)['data']['list']
+        for item in dongman_list:
+            thumb = item['cover']
+            link = item['link']
+            new_episode = int(re.findall('\d+', item['index_show'])[0])
+            is_vip = 1 if item['badge'] else 0
+            # 等价于以下写法
+            # if item['badge']:
+            #     is_vip = 0
+            # else:
+            #     is_vip = 0
+            nums = item['order']
+            title = item['title']
+            with open('b站番剧.txt', 'a+') as f:
+                f.write(json.dumps({'title':title, 'link':link,'new_episode':new_episode,'is_vip':is_vip,'nums':nums,'thumb':thumb,}))
+    pass
+
+
 if __name__ == '__main__':
     # json_demo()
-    urllib_demo()
+    # urllib_demo()
+    # requests_demo()
+    b_fanju_demo()
